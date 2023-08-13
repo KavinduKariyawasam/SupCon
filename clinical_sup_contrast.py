530d6af6d35fa3bf8aa8a66595509abfde24fb2a
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2023-08-13 15:05:43 +0530

    Add files via upload

---
+++ clinical_sup_contrast.py
@@ -0,0 +1,59 @@
+from config_supcon import parse_option
+from utils_supcon import set_loader,set_model_contrast
+from utils import set_optimizer, adjust_learning_rate,save_model
+import os
+import time
+import tensorboard_logger as tb_logger
+from training_one_epoch_trex import train_TREX
+from training_one_epoch_bio import train_Bio
+
+from training_one_epoch_oct import train_OCT
+from training_one_epoch_alpha import train_Alpha
+
+from training_one_epoch_prime import train_Prime
+from training_one_epoch_prime_trex_combined import train_Combined
+
+def main():
+    opt = parse_option()
+
+    # build data loader
+    train_loader = set_loader(opt)
+
+    # build model and criterion
+    model, criterion = set_model_contrast(opt)
+
+    # build optimizer
+    optimizer = set_optimizer(opt, model)
+
+    # tensorboard
+    logger = tb_logger.Logger(logdir=opt.tb_folder, flush_secs=2)
+
+    # training routine
+    for epoch in range(1, opt.epochs + 1):
+        adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+
+        if (opt.dataset == 'OCT'):
+            loss = train_OCT(train_loader, model, criterion, optimizer, epoch, opt)
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
+
+        # tensorboard logger
+        logger.log_value('loss', loss, epoch)
+        logger.log_value('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+
+        if epoch % opt.save_freq == 0:
+            save_file = os.path.join(
+                opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            save_model(model, optimizer, opt, epoch, save_file)
+
+    # save the last model
+    save_file = os.path.join(
+        opt.save_folder, 'last.pth')
+    save_model(model, optimizer, opt, opt.epochs, save_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils_supcon.py
@@ -0,0 +1,111 @@
+from torchvision import transforms, datasets
+from oct_dataset import OCTDataset
+from biomarker import BiomarkerDatasetAttributes
+from utils import TwoCropTransform
+from prime import PrimeDatasetAttributes
+from prime_trex_combined import CombinedDataset
+from recovery import recovery
+from trex import TREX
+
+from oct_cluster import OCTDatasetCluster
+import torch
+from resnet import SupConResNet
+from loss import SupConLoss
+import torch.backends.cudnn as cudnn
+try:
+    import apex
+    from apex import amp, optimizers
+except ImportError:
+    pass
+import torch.nn as nn
+def set_model_contrast(opt):
+
+
+    model = SupConResNet(name=opt.model)
+
+    criterion = SupConLoss(temperature=opt.temp,device=opt.device)
+    device = opt.device
+    # enable synchronized Batch Normalization
+    if opt.syncBN:
+        model = apex.parallel.convert_syncbn_model(model)
+
+    if torch.cuda.is_available():
+        if opt.parallel == 1:
+            model = torch.nn.DataParallel(model)
+            model = model.cuda()
+            criterion = criterion.cuda()
+        else:
+            model = model.to(device)
+            criterion = criterion.to(device)
+        cudnn.benchmark = True
+
+    return model, criterion
+
+
+def set_loader(opt):
+    # construct data loader
+    if opt.dataset == 'OCT':
+
+        mean = (.1904)
+        std = (.2088)
+    elif opt.dataset == 'Recovery' or opt.dataset == 'Recovery_Compressed':
+
+        mean = (.1706)
+        std = (.2112)
+    elif opt.dataset == 'path':
+
+        mean = eval(opt.mean)
+        std = eval(opt.std)
+    else:
+        raise ValueError('dataset not supported: {}'.format(opt.dataset))
+    normalize = transforms.Normalize(mean=mean, std=std)
+
+
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+        ], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+
+
+    if opt.dataset =='OCT':
+        csv_path_train = opt.train_csv_path
+        data_path_train = opt.train_image_path
+        train_dataset = OCTDataset(csv_path_train,data_path_train,transforms = TwoCropTransform(train_transform))
+    else:
+        raise ValueError(opt.dataset)
+    train_sampler = None
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=opt.batch_size, shuffle=True,
+        num_workers=opt.num_workers, pin_memory=True, sampler=train_sampler,drop_last=True)
+
+    return train_loader
+
+
+def set_model(opt):
+
+    model = SupConResNet(name=opt.model)
+
+    criterion = SupConLoss(temperature=opt.temp,device=opt.device)
+    device = opt.device
+    # enable synchronized Batch Normalization
+    if opt.syncBN:
+        model = apex.parallel.convert_syncbn_model(model)
+
+    if torch.cuda.is_available():
+        if opt.parallel == 1:
+            model = torch.nn.DataParallel(model)
+            model = model.cuda()
+            criterion = criterion.cuda()
+        else:
+            model = model.to(device)
+            criterion = criterion.to(device)
+        cudnn.benchmark = True
+
+    return model, criterion
