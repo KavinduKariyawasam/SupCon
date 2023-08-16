a8b5661325110f5870240ec9bd5e91dbeb74024f
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2023-08-16 10:29:05 +0530

    Update oct_dataset.py

--- oct_dataset.py
+++ oct_dataset.py
@@ -47,3 +47,19 @@ class BIOMARKERS(data.Dataset):
         b6 = self.df.iloc[idx, 6]
         bio_tensor = torch.tensor([b1, b2, b3, b4, b5, b6])
         return image, bio_tensor
+
+class RECOVERY(data.Dataset):
+    def __init__(self,df, img_dir, transforms):
+        self.img_dir = img_dir
+        self.transforms = transforms
+        self.df = pd.read_csv(df)
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        path = self.img_dir + self.df.iloc[idx,0]
+        image = Image.open(path).convert("L")
+        image = np.array(image)
+        image = Image.fromarray(image)
+        image = self.transforms(image)
+        return image
