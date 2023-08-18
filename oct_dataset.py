a1b73e7ac5e6a59a7536612e4f990c48d58332ee
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2023-08-18 10:47:11 +0530

    Update oct_dataset.py

--- oct_dataset.py
+++ oct_dataset.py
@@ -48,6 +48,27 @@ class BIOMARKERS(data.Dataset):
         bio_tensor = torch.tensor([b1, b2, b3, b4, b5, b6])
         return image, bio_tensor
 
+class TREX(data.Dataset):                                #new class created
+    def __init__(self,df, img_dir, transforms):
+        self.img_dir = img_dir
+        self.transforms = transforms
+        self.df = pd.read_csv(df)
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        path = self.img_dir + self.df.iloc[idx,0]
+        image = Image.open(path).convert("L")
+        image = np.array(image)
+        image = Image.fromarray(image)
+        image = self.transforms(image)
+        bcva = self.df.iloc[idx,1]
+        cst = self.df.iloc[idx,2]
+        eye_id = self.df.iloc[idx,3]
+        patient = self.df.iloc[idx, 4]
+        #bio_tensor = torch.tensor([b1, b2, b3, b4, b5, b6])
+        return image, bcva, cst, eye_id, patient
+
 class RECOVERY(data.Dataset):
     def __init__(self,df, img_dir, transforms):
         self.img_dir = img_dir
