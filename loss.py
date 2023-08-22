153a437410f2b2f31c71e47871752c93b3a89913
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2023-08-22 09:35:33 +0530

    SimCLR new

--- loss.py
+++ loss.py
@@ -3,6 +3,47 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+class SupConLoss(nn.Module):         #Contrastive loss with SimCLR
+   """
+   Vanilla Contrastive loss, also called InfoNceLoss as in SimCLR paper
+   """
+   def __init__(self, batch_size, temperature=0.5):
+       super().__init__()
+       self.batch_size = batch_size
+       self.temperature = temperature
+       self.mask = (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool)).float()
+
+   def calc_similarity_batch(self, a, b):
+       representations = torch.cat([a, b], dim=0)
+       return F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)
+
+   def forward(self, features):
+       """
+       proj_1 and proj_2 are batched embeddings [batch, embedding_dim]
+       where corresponding indices are pairs
+       z_i, z_j in the SimCLR paper
+       """
+       batch_size = feature.shape[0]
+       proj_1, proj_2 = torch.unbind(features, dim=1)
+       
+       z_i = F.normalize(proj_1, p=2, dim=1)
+       z_j = F.normalize(proj_2, p=2, dim=1)
+
+       similarity_matrix = self.calc_similarity_batch(z_i, z_j)
+
+       sim_ij = torch.diag(similarity_matrix, batch_size)
+       sim_ji = torch.diag(similarity_matrix, -batch_size)
+
+       positives = torch.cat([sim_ij, sim_ji], dim=0)
+
+       nominator = torch.exp(positives / self.temperature)
+
+       denominator = device_as(self.mask, similarity_matrix) * torch.exp(similarity_matrix / self.temperature)
+
+       all_losses = -torch.log(nominator / torch.sum(denominator, dim=1))
+       loss = torch.sum(all_losses) / (2 * self.batch_size)
+       return loss
+'''
 class SupConLoss(nn.Module):
     def __init__(self,device='cuda:0', temperature=0.07, contrast_mode='all',
                  base_temperature=0.07):
@@ -84,6 +125,8 @@ class SupConLoss(nn.Module):
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
+'''
+
 '''
 class SupConLoss(nn.Module):
     def __init__(self,device='cuda:0', temperature=0.07, contrast_mode='all',
